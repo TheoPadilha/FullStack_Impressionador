b97276a12a70a04c87edfd627d361f09360164d1
TheoPadilha <168146283+TheoPadilha@users.noreply.github.com>
2025-06-11 09:41:04 -0300

    commit

--- aulas_js/exercicios/exe8.js
+++ aulas_js/exercicios/exe8.js
@@ -1,8 +1,10 @@
 //  Exercício 8: Declare uma variável para armazenar o nome de uma cidade e outra para
 //  armazenar a sua população. Exiba uma mensagem combinando ambas as variáveis
 
-let cidade = "Balneario Camboriú"
-let população = 200000
+let cidade = "Balneario Camboriú";
+let população = 200000;
 
-console.log("á população da cidade de " + cidade + " é de " + população + " habitanates")
-console.log(`A cidade de ${cidade} tem ${população} habitantes`)
+console.log(
+  "á população da cidade de " + cidade + " é de " + população + " habitanates"
+);
+console.log(`A cidade de ${cidade} tem ${população} habitantes`);
